66e2638cacc95aa72ee81fb9dcf76a5ecfe5c156
Devin Nemec <devin.nemec@gmail.com>
2017-08-21 15:03:59 -0500

    Generate a single test message to the #general channel

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	const hookGen = "https://hooks.slack.com/services/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"
+	const json = "application/json"
+	var payload = "test message"
+
+	if len(os.Args) > 1 {
+		payload = strings.Join(os.Args[1:], " ")
+	}
+	var body = []byte(`{"text":"` + payload + `"}`)
+	fmt.Printf(string(body[:]))
+	req, err := http.Post(hookGen, json, bytes.NewBuffer(body))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer req.Body.Close()
+	fmt.Printf("Response Code:", req.StatusCode)
 }
