f03f42143942cd3f9332664fb064386cb5862cc0
Devin Nemec <devin.nemec@gmail.com>
2017-08-22 16:57:40 -0500

    Retrieve basecamp events and post a message to slack. Next up is putting the two together.

--- main.go
+++ main.go
@@ -3,18 +3,29 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	//	"net/url"
 	"os"
 	"strings"
+	"time"
 )
 
-const hookGen = "https://hooks.slack.com/services/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"
-const json = "application/json"
-const basecampEventsURL = ""
+const (
+	hookGen          = "https://hooks.slack.com/services/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"
+	json             = "application/json"
+	bcmpApi          = "https://basecamp.com/"
+	bcmpApiVer       = "/api/v1/"
+	bcmpApiEvnt      = "events.json?since="
+	userAgent        = "Agent-Smith (devin.nemec@banno.com)"
+	userAgentVersion = "0.1"
+	uaString         = userAgent + userAgentVersion
+)
 
 func main() {
 	var message string
+	var timestamp time.Time
 	if len(os.Args) > 1 {
 		message = strings.Join(os.Args[1:], " ")
 	}
@@ -23,6 +34,8 @@ func main() {
 	if status != 200 {
 		fmt.Println("The message was not sent")
 	}
+
+	getEvents(timestamp)
 
 }
 
@@ -41,26 +54,66 @@ func postGeneral(payload string) int {
 }
 
 // Retreives all events from Basecamp
-func getEvents() {
+func getEvents(sinceT time.Time) {
 	//TODO add "since" field for URL
+	var since string
+	if sinceT.IsZero() {
+		startTime := time.Now()
+		//2012-03-24T11:00:00-06:00 RFC3339?
+		//2006-01-02T15:04:05Z07:00 RFC3339
+		//hour := time.Duration(1) * time.Hour
+		startTime = startTime.Add(-1 * time.Hour)
+		sinceB, err := startTime.MarshalText()
+		if err != nil {
+			log.Fatal(err)
+		}
+		since = string(sinceB[:])
+		since = strings.Trim(since, `"`)
+	}
+
+	//since = "2017-08-22T13:59:13.000-06:00"
+	//since = "2017-08-22T13:34:20.245234-06:00"
 
 	//make new client
 	client := &http.Client{}
 
 	//don't thnk I need this line
 	//resp, err := http.Get(basecampEventsURL)
+	bcmpId := os.Getenv("BASECAMP_ID")
+	bcmpApiUrl := bcmpApi + bcmpId + bcmpApiVer + bcmpApiEvnt
+
+	fmt.Println(bcmpApiUrl + since)
 
 	// build new request
-	req, err := http.NewRequest("GET", basecampEventsURL, nil)
+	req, err := http.NewRequest("GET", bcmpApiUrl+since, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// add basic auth
+	userBcmp := os.Getenv("BASECAMP_USER")
+	passBcmp := os.Getenv("BASECAMP_PASS")
+	req.SetBasicAuth(userBcmp, passBcmp)
 
 	// add additional headers
-	req.Header.Add("User-Agent", "Agent-Smith (devin.nemec@banno.com)")
+	req.Header.Add("User-Agent", uaString)
+	req.Header.Add("Content-Type", json)
 
 	// send request
 	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	// close request
 	defer resp.Body.Close()
 
-	fmt.Printf(resp.Body)
+	// print body
+	fmt.Println("Basecamp Response Status Code: " + resp.Status)
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	bs := string(body)
+	fmt.Print(bs)
 }
