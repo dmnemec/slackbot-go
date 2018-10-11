d8696d071b5da38402a438e3621338be0c411977
Devin Nemec <devin.nemec@banno.com>
2018-10-11 15:21:08 -0500

    Get client with conversations API in it pushed up

--- client/client.go
+++ client/client.go
@@ -1,18 +1,8 @@
 package client
 
 import (
-	//Global
-	"context"
-	"fmt"
-	"log"
-	"os"
-
 	//Local
 	"github.com/DMNemec/slackbot-go/conversations"
-
-	//Remote
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/slack"
 )
 
 // Client is a total Slack API client
@@ -22,38 +12,13 @@ type Client struct {
 }
 
 func (c *Client) new(token string) *Client {
+	c = &Client{}
 	c.Client = conversations.NewClient(token)
 	c.Token = token
 	return c
 }
 
 // New creates a new Slack API Client struct with an OAuth token
-func New() (client *Client) {
-	ctx := context.Background()
-	conf := &oauth2.Config{
-		ClientID:     os.Getenv("workspace_ID"),
-		ClientSecret: os.Getenv("workspace_token"),
-		Scopes:       []string{"conversations:read", "conversations:write"},
-		Endpoint:     slack.Endpoint,
-	}
-
-	// Redirect user to consent page to ask for permission
-	// for the scopes specified above.
-	url := conf.AuthCodeURL("state", oauth2.AccessTypeOffline)
-	fmt.Printf("Visit the URL for the auth dialog: %v", url)
-
-	// Use the authorization code that is pushed to the redirect
-	// URL. Exchange will do the handshake to retrieve the
-	// initial access token. The HTTP Client returned by
-	// conf.Client will refresh the token as necessary.
-	var code string
-	if _, err := fmt.Scan(&code); err != nil {
-		log.Fatal(err)
-	}
-	tok, err := conf.Exchange(ctx, code)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return client.new(tok.AccessToken)
+func New(token string) (client *Client) {
+	return client.new(token)
 }
